18ee4cccdc0aa87f21b96828df177b3552507450
NatTheBat <natacha.agafonov@gmail.com>
2017-10-12 15:53:17 +0200

    tentative d'imbrication des 2 méthodes, pour l'instant sans succès

--- test.rb
+++ test.rb
@@ -9,17 +9,18 @@ def get_all_the_urls_of_val_doise_townhalls()
 		liste = links.each{|departement| puts 'http://annuaire-des-mairies.com/' + departement['href']}
 end
 
-get_all_the_urls_of_val_doise_townhalls 
-
-
-
-
-
-page2 = Nokogiri::HTML(open())
-page2.css('p:contains("@")').each do |email|
-puts email.text
+def  get_the_email_of_a_townhal_from_its_webpage(url)
+	page2 = Nokogiri::HTML(open(url))
+	page2.css('p:contains("@")').each do |email|
+	puts email.text
+	end
 end
 
+tableau = get_all_the_urls_of_val_doise_townhalls 
+
+tableau.each do |url|
+	get_the_email_of_a_townhal_from_its_webpage(url) 
+end
 
 
 
