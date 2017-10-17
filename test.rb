3798d94fbfd546c978bea4bd74c0b7e5c2920975
NatTheBat <natacha.agafonov@gmail.com>
2017-10-17 14:41:35 +0200

    récupération des emails

--- test.rb
+++ test.rb
@@ -8,30 +8,27 @@ def get_all_the_urls_of_val_doise_townhalls()
 		tableau = []
 		page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 		links = page.css("a.lientxt")
-		links.each{|departement| puts 'http://annuaire-des-mairies.com/' + departement['href']}
+		liste = links.each{|departement| tableau << 'http://annuaire-des-mairies.com/' + departement['href']}
+
 		return tableau
+
 end
 
 tableau = get_all_the_urls_of_val_doise_townhalls
 
+
 #Récupérer l'email de la mairie dans une page donnée
 
-def  get_the_email_of_a_townhal_from_its_webpage
-	page2 = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/ableiges.html"))
+
+def  get_the_email_of_a_townhall_from_its_webpage(page_url)
+	page2 = Nokogiri::HTML(open(page_url))
 	page2.css('p:contains("@")').each do |email|
 	puts email.text
 	end
 end
 
-get_the_email_of_a_townhal_from_its_webpage
-
-#tentative infructueuse de combiner les 2
-=begin
+#assembler les 2
 tableau.each do |url|
-	get_the_email_of_a_townhal_from_its_webpage(url) 
+	get_the_email_of_a_townhall_from_its_webpage(url) 
 end
-=end
-
-
-
 	
